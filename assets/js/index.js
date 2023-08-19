87bebcb466ad0ad597db70f86260d1aba5d75c58
Ed Cetina <eocetina@gmail.com>
2023-08-18 19:42:05 -0600

    limpieza de codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,22 @@
-function adivinaNumero() {
+
+let midNumber=(lim_max,lim_min)=> {
+  return  Math.floor((lim_max + lim_min) / 2); //seleccionamos el elemento de en medio
+ }
+
   let midNum= 0;
   let lim_min = 0;
   let lim_max = 100;
   let flag = false; // flag para
  
   
-  while(flag!){
+  while(!flag){
     midNum =midNumber(lim_max,lim_min);// buscamos el elemento de en medio.
     if(confirm(`tu num es ${midNum}?`)){
     document.getElementById("respuesta").innerHTML = `tu num es: ${midNum}`; // imprimimos en el dom el numero ganador
     flag=true;
     break;
     }else{
-      if(confirm(`El numero es mayor a (>) ${midNum}?`){
+      if(confirm(`El numero es mayor a (>) ${midNum}?`)){
         lim_min=midNum;
       }else{
         lim_max=midNum;
@@ -21,33 +25,4 @@ function adivinaNumero() {
     }
 
   }
-  
-
-
-    // if (confirm(`tu num se encuentra entre ${midNumber} y ${lim_max}?`)) {
-    //   lim_min=midNumber;
-    //   midNumber=lim_max - Math.floor((lim_max - lim_min) / 2);
-    //   if (confirm(`${text} ?`)) { // tu numero es X?
-    //     // confirmar elemento adivinado
-    //     document.getElementById("respuesta").innerHTML = text;
-    //     flag = true;
-    //   }
-    // } else {
-    //   // Caso donde el numero se encuentra en el lado izquierdo de los numeros 
-    //   lim_max=m(midNumber-1)-lim_min;
-    //   midNumber=Math.floor((lim_max-lim_min)/2);
-    //   if (confirm(`${text} ?`)) { // tu numero es X?
-    //     // confirmar elemento adivinado
-    //     document.getElementById("respuesta").innerHTML = text;
-    //     flag = true;
-    //   }
-
-    // }
  
-}
-
-
-function midNumber(lim_max,lim_min){
- 
-return  Math.floor((lim_max - lim_min) / 2); //seleccionamos el elemento de en medio
-}
